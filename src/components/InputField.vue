96be99c4f45bb02cb73e30c04e57bf5c0081f27f
yaserbayat <babak.kafka@gmail.com>
2023-01-12 00:04:27 +0330

    feat: add tailwind

---
+++ src/components/InputField.vue
@@ -0,0 +1,30 @@
+<template>
+  <label :for="name" class="flex flex-col mb-4">
+    <span class="mb-1 font-bold text-sm">{{ label }}</span>
+    <input
+      :type="type"
+      :id="name"
+      :name="name"
+      class="rounded-md border h-8 px-2"
+    />
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+export default defineComponent({
+  name: "InputField",
+  props: {
+    label: { require: true, type: String as PropType<string> },
+    name: { require: true, type: String as PropType<string> },
+    type: {
+      require: true,
+      type: String as PropType<string>,
+      defaultValue: "text",
+    },
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import InputField from "../components/InputField.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { InputField },
+  setup() {
+    const onSubmit = (e: any) => {
+      console.log(e);
+    };
+    return { onSubmit };
+  },
+});
+</script>
 
 <template>
-  <div>Home</div>
+  <div class="h-screen flex justify-center items-center">
+    <form
+      @onsubmit="onSubmit"
+      class="flex flex-col w-full md:w-1/2 mx-auto py-2 px-3 border rounded-md mt-1/2 max-w-screen-sm shadow-md"
+    >
+      <h1 class="text-center text-lg font-bold">Welcome</h1>
+      <InputField type="text" label="Username" name="username" />
+      <InputField type="password" label="password" name="password" />
+    </form>
+  </div>
 </template>
