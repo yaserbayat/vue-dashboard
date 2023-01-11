a9b7f12ac58451db1c8619b16e4e8367dadba5d6
yaserbayat <babak.kafka@gmail.com>
2023-01-12 00:55:04 +0330

    feat: add login form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,15 @@ import { defineComponent } from "vue";
 export default defineComponent({
   components: { InputField },
   setup() {
-    const onSubmit = (e: any) => {
-      console.log(e);
+    const onSubmit = (e: Event) => {
+      const target = e.target as HTMLFormElement;
+      const formData = new FormData(target);
+      const data = {};
+      // need to convert it before using it
+      for (let [key, val] of formData.entries()) {
+        Object.assign(data, { [key]: val });
+      }
+      console.log(data);
     };
     return { onSubmit };
   },
@@ -16,12 +23,15 @@ export default defineComponent({
 <template>
   <div class="h-screen flex justify-center items-center">
     <form
-      @onsubmit="onSubmit"
+      v-on:submit.prevent="(e) => onSubmit(e)"
       class="flex flex-col w-full md:w-1/2 mx-auto py-2 px-3 border rounded-md mt-1/2 max-w-screen-sm shadow-md"
     >
       <h1 class="text-center text-lg font-bold">Welcome</h1>
       <InputField type="text" label="Username" name="username" />
       <InputField type="password" label="password" name="password" />
+      <button type="submit" class="h-8 bg-blue-500 text-white rounded-md">
+        Submit
+      </button>
     </form>
   </div>
 </template>
